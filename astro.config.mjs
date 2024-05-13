9af9d11109b5ae4b66d34060d0526709767c13b4
Marcelo Mossian <mmossian@gmail.com>
2024-05-13 18:27:28 -0300

    Version 1.0 CV

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+
+// import svelte from "@astrojs/svelte";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind()], //, svelte()
+  i18n: {
+    defaultLocale: "es",
+    locales: ["es", "en"],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
+});
